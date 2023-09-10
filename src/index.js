ace97a4ad49cab5bc502a630b7a84b663f1acd9f
ekrat123 <dilekmehmet123@gmail.com>
2023-09-10 10:20:14 +0300

    files for project added and wepback configured.

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import "./style.css";
+import background from "./images/background.jpg";
+
+/* const myBackground = new Image();
+myBackground.src = background;
+
+const containerEl = document.querySelector("#container");
+containerEl.appendChild(myBackground); */
